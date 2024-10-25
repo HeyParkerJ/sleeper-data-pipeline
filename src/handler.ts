6d19770f03f58f739e0264dea49d0f07d355e28f
Parker Johnson <parkerjohnsonwebdev+gitconfig@gmail.com>
2024-10-25 17:02:10 -0600

    zip the dist

--- src/handler.ts
+++ src/handler.ts
@@ -1,6 +1,6 @@
 import { APIGatewayEvent, Context } from 'aws-lambda';
 
-export const handler = async (event: APIGatewayEvent, context: Context) => {
+export const lambda_handler = async (event: APIGatewayEvent, context: Context) => {
     return {
         statusCode: 200,
         body: JSON.stringify({ message: "Hello, TypeScript Lambda!" }),
