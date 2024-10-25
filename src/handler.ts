e9431d21f4cf83b67b07cc779f46550774f89606
Parker Johnson <parkerjohnsonwebdev+gitconfig@gmail.com>
2024-10-25 13:36:27 -0600

    initial commit

---
+++ src/handler.ts
@@ -0,0 +1,9 @@
+import { APIGatewayEvent, Context } from 'aws-lambda';
+
+export const handler = async (event: APIGatewayEvent, context: Context) => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Hello, TypeScript Lambda!" }),
+    };
+};
+
